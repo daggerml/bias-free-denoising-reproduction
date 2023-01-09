a85aac0654c5c8f0b4b6594689b12123abe2e2f2
Aaron Niskin <amniskin@gmail.com>
2023-01-09 01:29:59 -0600

    wip

--- play.py
+++ play.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
-import boto3
 import daggerml as dml
-import executor_s3 as s3
+import executor_s3 as s3  # noqa: F401
 from pathlib import Path
 from pprint import pprint
-from urllib.parse import urlparse
 
 
 IMAGE_ROOT = Path(__file__).parent / 'submodules/bias_free_denoising/data'
@@ -12,10 +10,16 @@ IMAGE_ROOT = Path(__file__).parent / 'submodules/bias_free_denoising/data'
 
 if __name__ == '__main__':
     with dml.Dag.new('bfdn.v0.dev') as dag:
-        n = dag.load('bfdn.v0.data')
-        print('n ==', n)
+        print('dag executor:', dag.executor)
         print()
-        print('n["test"] ==', n['test'])
+        data = dag.load('bfdn.v0.data')
+        print('data ==', data)
         print()
-        pprint(n.to_py())
-        print(1/0)
+        print('data["test"] ==', data['test'])
+        print()
+        pprint(data.to_py())
+        print()
+        print()
+        print('k8s:', dag.load('com.daggerml.resource.k8s').to_py())
+        print()
+        print('docker-build:', dag.load('com.daggerml.resource.docker').to_py())
